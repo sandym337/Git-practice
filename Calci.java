9d10291d6e9410b8e809d8b859de8b4076decded
sandym337 <sandhyamekala337@gmail.com>
2024-01-11 12:14:15 +0530

    implementing the interface

---
+++ Calci.java
@@ -0,0 +1,14 @@
+public class Calci{
+public void add(int a,int b)
+{
+int c=a+b;
+int d=c-b;
+system.out.println(c);
+system.out.println(d);
+}
+public static viod main(String args[])
+{
+Calci c=new Calci();
+c.add(2,3);
+}
+}
